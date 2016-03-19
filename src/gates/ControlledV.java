2de3872e24a10afb3c89698fffc667f90a9e06bc
minhao-he <358276312@qq.com>
2016-03-19 15:25:28 +0000

    used in the toffoli gate

---
+++ src/gates/ControlledV.java
@@ -0,0 +1,14 @@
+package gates;
+
+import mathStructs.Complex;
+import mathStructs.Matrix;
+
+public class ControlledV extends Gate {
+    public ControlledV(){
+        gate=new Matrix(4,4);
+        gate.setElement(0,0,1.0);
+        gate.setElement(1,1,1.0);
+        gate.setElement(2,2,1.0);
+        gate.setElement(3,3,new Complex(0, 1));
+    }
+}
