16f8e382050575a6946015ffff37815d0e6c015c
minhao-he <358276312@qq.com>
2016-03-19 15:24:31 +0000

    a minor mistake in the gate

--- src/gates/CNOT.java
+++ src/gates/CNOT.java
@@ -8,6 +8,6 @@ public class CNOT extends Gate {
         gate.setElement(0, 0, 1.0);
         gate.setElement(1, 1, 1.0);
         gate.setElement(2, 3, 1.0);
-        gate.setElement(2, 3, 1.0);
+        gate.setElement(3, 2, 1.0);
     }
 }
